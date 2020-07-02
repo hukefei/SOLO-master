fcc034470ea7b4dee39d6071d179695df8a3a61b
KEFEI.HU <hukeroy@163.com>
2020-07-02 18:43:43 +0800

    add

--- inference.py
+++ inference.py
@@ -2,9 +2,9 @@ from mmdet.apis import init_detector, inference_detector, show_result_pyplot, sh
 import mmcv
 import os, glob, time
 
-config_file = 'cfg/aug_solov2.py'
+config_file = 'cfg/aug_solov2_tuned.py'
 # download the checkpoint from model zoo and put it in `checkpoints/`
-checkpoint_file = './work_dirs/aug_solov2_add/epoch_1.pth'
+checkpoint_file = 'work_dirs/solov2_tianchi_tuned.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
@@ -15,10 +15,10 @@ model = init_detector(config_file, checkpoint_file, device='cuda:0')
 # show_result_ins(img, result, model.CLASSES, score_thr=0.25,
 #                     out_file='./WechatIMG14_out.jpeg')
 
-imgs = glob.glob('./test_imgs/*.*')
+imgs = glob.glob('/workspace/tianchi/tianchiyusai/JPEGImages/621838/*.jpg')
 # imgs = ['./test_imgs/14755.jpg']
-imgs = glob.glob('/home/versa/dataset/MSCOCO/aug_seg/val_imgs/*.*')
-save_dir = './aug_solov2_add'
+# imgs = glob.glob('/home/versa/dataset/MSCOCO/aug_seg/val_imgs/*.*')
+save_dir = './result'
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
